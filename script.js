ef09ee0e21c9bbbcb6eec54c0ae0177341348b03
system1novatama <system1@novatama.co.id>
2015-01-06 09:47:02 +0700

    Poppulady

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "Ella Ufaira Shop"; // NAMA TOKO ONLINE
-	var domain = "http://ellaufairashop.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Poppulady"; // NAMA TOKO ONLINE
+	var domain = "http://poppulady.tokomobile.link/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/Ella-Ufaira-Shop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "7cd953587cd0cafb5a3fcd7fe83aab60"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/Poppulady"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "359f91fa827a388154f6259be0d6e4c4"; // ISI DENGAN TOKEN 
 	
 	var cart_item_id = new Array();
 	var cart_item_qty = new Array();
