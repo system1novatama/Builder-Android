e927a63c94943e5c534cf1bfc28fd4871c2e3d5a
system1novatama <system1@novatama.co.id>
2015-01-19 11:12:01 +0700

    zoel collection

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "SHIINE ONLINESHOP"; // NAMA TOKO ONLINE
-	var domain = "http://shine-onlineshop.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Zoel Collection"; // NAMA TOKO ONLINE
+	var domain = "http://zoel-collection-onlineshop.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/SHIINEONLINESHOP"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "c0394b25fb5a4f458c3efed2448eda7f"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/ZoelCollection"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "1158388e8375892e1ee7dd7a5296d3fa"; // ISI DENGAN TOKEN 
 	
 	var cart_item_id = new Array();
 	var cart_item_qty = new Array();
