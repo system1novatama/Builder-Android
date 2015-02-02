babb9b0fc5bcf900174659f3564e3e939ceb5e45
system1novatama <system1@novatama.co.id>
2015-02-02 16:41:14 +0700

    mrs han pindah server

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "Gerobak Ummi"; // NAMA TOKO ONLINE
-	var domain = "http://gerobakummi.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Mrs Han"; // NAMA TOKO ONLINE
+	var domain = "http://mrs-han.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/GerobakUmmi"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "c7b21bdb6c77029118239b3ba6e01d05"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/Mrs-Han"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "6e61df1a066cc22f94c1c81987fe9526"; // ISI DENGAN TOKEN 
 	
 	var cart_item_id = new Array();
 	var cart_item_qty = new Array();
