f3ef91b6ca6f2eb905c30100e9aee249f540e822
system1novatama <system1@novatama.co.id>
2015-02-04 20:35:32 +0700

    yes fashion

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "Hijaber Premium Boutique"; // NAMA TOKO ONLINE
-	var domain = "http://hijaberpremium.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "YES FASHION"; // NAMA TOKO ONLINE
+	var domain = "http://yesfashion-olshop.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/HijaberPremiumBoutique"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "d18c94aecae6e2167717d56ee2e600d4"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/YESFASHION"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "675939019c5177f0d269ad85bae8bf98"; // ISI DENGAN TOKEN 
 	
 	var cart_item_id = new Array();
 	var cart_item_qty = new Array();
