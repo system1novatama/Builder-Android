fadf6f49e6378afa2ca6474ff2c23ceb89939ced
system1novatama <system1@novatama.co.id>
2015-02-09 15:13:17 +0700

    chuculatte

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "BSO"; // NAMA TOKO ONLINE
-	var domain = "http://tykhe-store.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Chuculatte"; // NAMA TOKO ONLINE
+	var domain = "http://chuculattecucu.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/BSO"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "f878729157be2ac8d686bfca35f38d32"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/Chuculatte"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "cbb89e74cd6cb7afcb1cf034549d4dbb"; // ISI DENGAN TOKEN 
 	
 	var cart_item_id = new Array();
 	var cart_item_qty = new Array();
