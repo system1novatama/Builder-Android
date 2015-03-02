6aa4d4766be5e156ab8eff32444ffd1f66ca7964
system1novatama <system1@novatama.co.id>
2015-03-02 19:42:49 +0700

    f2-fasion

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "Tokomobile Demo"; // NAMA TOKO ONLINE
-	var domain = "http://tokomobile.co.id/demo/"; // DOMAIN URL ADMIN
+	var shop_name = "F2 Fashion"; // NAMA TOKO ONLINE
+	var domain = "http://f2-fashion.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/TokomobileDemo"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "2897218422421213"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/F2Fashion"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "0f264b0f53d7835095c9a29e036d964e"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
