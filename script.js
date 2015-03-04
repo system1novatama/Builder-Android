3a215729ad480bdd92294bb1c136a9370039fcb5
system1novatama <system1@novatama.co.id>
2015-03-04 14:17:11 +0700

    pdf replace

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "Happy Ricci Collection"; // NAMA TOKO ONLINE
-	var domain = "http://happyricci.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "PDF Grosir"; // NAMA TOKO ONLINE
+	var domain = "http://primadewatafashion.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/HappyRicciCollection"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "e8d9a3c7d15189efe5e409c334b9f629"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/PDFGrosir"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "7e2f4c3e5737f2799ddda918cb67da13"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
