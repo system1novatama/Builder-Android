cc8ddf8a5e12371e1c7984585ba0edea371fea1d
system1novatama <system1@novatama.co.id>
2015-03-09 14:20:42 +0700

    mrs-han replace

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "Happy Ricci Collection"; // NAMA TOKO ONLINE
-	var domain = "http://happyricci.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Mrs Han"; // NAMA TOKO ONLINE
+	var domain = "http://mrs-han.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/HappyRicciCollection"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "e8d9a3c7d15189efe5e409c334b9f629"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/MrsHan"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "6e61df1a066cc22f94c1c81987fe9526"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
