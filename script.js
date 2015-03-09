924bcb46856c1ca7db0ec14503808582fa2a5865
system1novatama <system1@novatama.co.id>
2015-03-09 13:36:20 +0700

    cheryl shop replace

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "Darel Fashion"; // NAMA TOKO ONLINE
-	var domain = "http://darel-fashion.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Cheryl Shoppe"; // NAMA TOKO ONLINE
+	var domain = "http://cheryl-shoppe.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/DarelFashion"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "eded8ad40304c17fbcc8aa5635db8df3"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/CherylShoppe"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "720bfd7dc55880307a1d5479fcca5fb6"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
