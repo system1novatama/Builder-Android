c34256d2ef0b788a77a6266321959e5006e280e4
system1novatama <system1@novatama.co.id>
2015-03-09 09:40:42 +0700

    ellaufairah replace 2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "Tokomobile Demo"; // NAMA TOKO ONLINE
-	var domain = "http://tokomobile.co.id/demo/"; // DOMAIN URL ADMIN
+	var shop_name = "Ella Ufaira Shop"; // NAMA TOKO ONLINE
+	var domain = "http://ellaufairashop.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/TokomobileDemo"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "2897218422421213"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/Ella-Ufaira-Shop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "7cd953587cd0cafb5a3fcd7fe83aab60"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
