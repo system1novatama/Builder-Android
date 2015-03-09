f7b9ef310cbb0fcf0e695111fc59d8e051934740
system1novatama <system1@novatama.co.id>
2015-03-09 15:11:44 +0700

    zoel collection replace

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "JE Clothing"; // NAMA TOKO ONLINE
-	var domain = "http://je-clothing.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Zoel Collection"; // NAMA TOKO ONLINE
+	var domain = "http://zoel-collection-onlineshop.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/JE-Clothing"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "5fb00655381b1e417ec4a4e7de9682d9"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/ZoelCollection"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "1158388e8375892e1ee7dd7a5296d3fa"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
