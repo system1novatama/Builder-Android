94d655209ce236bc37e1c4768a4f64ee68d89fea
system1novatama <system1@novatama.co.id>
2015-03-10 11:11:29 +0700

    epic replace2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "Toko Sachio"; // NAMA TOKO ONLINE
-	var domain = "http://tokosachio.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "EPIC"; // NAMA TOKO ONLINE
+	var domain = "http://epicfashion.id/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/TokoSachio"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "7698e7424252eaeee4b9f31c9552871f"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/EPIC"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "f5d17c6f4182a8690d3277680e3ee32d"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
