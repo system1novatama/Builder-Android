48b1dbc9591f2be59eb8242769434664803df743
system1novatama <system1@novatama.co.id>
2015-03-12 11:40:21 +0700

    mieke patch

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "Tokomobile Demo"; // NAMA TOKO ONLINE
-	var domain = "http://tokomobile.co.id/demo/"; // DOMAIN URL ADMIN
+	var shop_name = "Mieke Online Shop"; // NAMA TOKO ONLINE
+	var domain = "http://miekeonlineshop.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "TokomobileDemo"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "2897218422421213"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/MiekeOnlineShop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "2473463626a950916789c14dd974ab26"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
