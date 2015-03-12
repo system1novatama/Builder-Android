1a10fec5c3093dac77cbcddb302abd8dd3d20202
system1novatama <system1@novatama.co.id>
2015-03-12 12:16:41 +0700

    toko3 coba

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "Mieke Online Shop"; // NAMA TOKO ONLINE
-	var domain = "http://miekeonlineshop.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Tokomobile Demo"; // NAMA TOKO ONLINE
+	var domain = "http://tokomobile.co.id/demo/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/MiekeOnlineShop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "2473463626a950916789c14dd974ab26"; // ISI DENGAN TOKEN 
+	var dir_image = "DCIM/TokomobileDemo"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "2897218422421213"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
@@ -2845,7 +2845,7 @@ var cart_item_id = new Array();
 	function download(file_img, Folder_Name, base_download_url) {
 	//step to request a file system 
 		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSystemSuccess, fileSystemFail);
-
+		/*
 		function fileSystemSuccess(fileSystem) {
 			var download_link = encodeURI(base_download_url+"download_img.php?file_img="+file_img);
 			ext = download_link.substr(download_link.lastIndexOf('.') + 1); //Get extension of URL
@@ -2859,7 +2859,26 @@ var cart_item_id = new Array();
 			// download function call
 			filetransfer(download_link, fp);
 		}
-
+		*/
+		function fsSuccess(fs){
+		if (fs.root.fullPath === 'file:///storage/sdcard0'){
+		fs.root.fullPath = 'file:///storage'; // change the path
+		}
+		// create directory reader
+		var directoryReader = fs.root.createReader()
+		// get a list of all entries in the directory
+		directoryReader.readEntries(dirSuccess,fail);
+		}
+		 
+		function dirSuccess(entries){
+		console.log(entries);
+		// will print something like the following to the console
+		// [{"isFile":false,"isDirectory":true,"name":"extSdCard",
+		// "fullPath":"file:///storage/extSdCard","filesystem":null},
+		// {"isFile":false,"isDirectory":true,"name":"sdcard0",
+		// "fullPath":"file:///storage/sdcard0","filesystem":null}
+		// ]
+		}
 		function onDirectorySuccess(parent) {
 			// Directory created successfuly
 		}
