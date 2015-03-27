190c6b2c024f3d2b3347744b3346d8b1ac210f60
system1novatama <system1@novatama.co.id>
2015-03-27 10:50:47 +0700

    Revert "clauidia online"
    This reverts commit 298a37d5d5112a609fb8aeffb92ec18d12b5c180.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "Claudia Collection"; // NAMA TOKO ONLINE
-	var domain = "http://claudiaonlineshop.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "MissEka Shop"; // NAMA TOKO ONLINE
+	var domain = "http://misseka.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/ClaudiaCollection"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "ede7df7543f5f9ea35a00f0ca6d93acc"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/MissEkaShop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "a77d72b5c411c669458dc384b2bd998d"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
