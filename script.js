f3903b65a9556a9ddece264a8b4730105ad3f9f4
system1novatama <system1@novatama.co.id>
2015-03-31 14:59:55 +0700

    colaino

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "SG"; // NAMA TOKO ONLINE
-	var domain = "http://sugar-onlineshop.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Colaino Mode"; // NAMA TOKO ONLINE
+	var domain = "http://colainomode.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/SG"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "f30d960bf55a698d1252e98fa0b73f92"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/ColainoMode"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "92f114efd7cd534df975b738ece01ecd"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
